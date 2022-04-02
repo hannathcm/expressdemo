5e07f4c2b6cd5bdcfa875deac333a835ca203fda
hannathcm <81477074+hannathcm@users.noreply.github.com>
2022-04-02 14:08:07 +0400

    Update router.js

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ response: "Server is up and running." }).status(200);
+  res.send('<h1>Hello World</h1>');
 });
 
 module.exports = router;
