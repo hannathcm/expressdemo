7538e1a457403537a40cd551d26ea3f3cafaa2e7
hannathcm <81477074+hannathcm@users.noreply.github.com>
2022-04-02 13:36:50 +0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
+    res.end('<h1>Hell</h1>');
 });
 
 const io = new Server(server, {
